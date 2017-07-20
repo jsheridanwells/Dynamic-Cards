1f988f2afb3947ea72dffd47477e5450f3f86761
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-07-20 13:03:34 -0500

    basic functionality

--- cards.js
+++ cards.js
@@ -7,18 +7,25 @@ let addBtn = document.getElementById('add-button');
 let count = 0;
 
 function makeCard() {
-  count++;
-  let text = input.value;
-  let newCard = document.createElement('div');
-  let delBtn = document.createElement('button');
+  count++;  //increase counter
+  let text = input.value;  //get text from textbox
+  let newCard = document.createElement('div');  //create new card
+  let delBtn = document.createElement('button');  //create delete button
 
   newCard.className = 'card';
   delBtn.className = 'delete-button';
   delBtn.innerText = 'Delete';
 
-  newCard.innerHTML = `<h2 class="card-title">Card ${count}</h2><p>${text}</p>`;
-  newCard.appendChild(delBtn);
-  frame.appendChild(newCard);
+  newCard.innerHTML = `<h2 class="card-title">Card ${count}</h2><p>${text}</p>`;  //insert content
+  newCard.appendChild(delBtn);  //add delete button to card
+  frame.appendChild(newCard);  //add card to frame
+  input.value = '';  //clear text in textbox
+
+  delBtn.addEventListener('click', deleteCard);
+
+  function deleteCard() {
+    frame.removeChild(newCard);  //remove last card from frame
+  }
 }
 
 addBtn.addEventListener('click', makeCard);
